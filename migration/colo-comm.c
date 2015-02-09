4c67c5947b41563a6e4d64568d02947e619974a8
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-02-09 17:39:01 +0000

    COLO: migrate colo related info to slave
    We can know if VM in destination should go into COLO mode by refer to
    the info that has been migrated from PVM.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Yang Hongyang <yanghy@cn.fujitsu.com>
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>

---
+++ migration/colo-comm.c
@@ -0,0 +1,55 @@
+/*
+ * COarse-grain LOck-stepping Virtual Machines for Non-stop Service (COLO)
+ * (a.k.a. Fault Tolerance or Continuous Replication)
+ *
+ * Copyright (c) 2015 HUAWEI TECHNOLOGIES CO.,LTD.
+ * Copyright (c) 2015 FUJITSU LIMITED
+ * Copyright (c) 2015 Intel Corporation
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or
+ * later. See the COPYING file in the top-level directory.
+ *
+ */
+
+#include <migration/migration-colo.h>
+
+#define DEBUG_COLO_COMMON 0
+
+#define DPRINTF(fmt, ...)                                  \
+    do {                                                   \
+        if (DEBUG_COLO_COMMON) {                           \
+            fprintf(stderr, "COLO: " fmt, ## __VA_ARGS__); \
+        }                                                  \
+    } while (0)
+
+static bool colo_requested;
+
+/* save */
+static void colo_info_save(QEMUFile *f, void *opaque)
+{
+    qemu_put_byte(f, migrate_enable_colo());
+}
+
+/* restore */
+static int colo_info_load(QEMUFile *f, void *opaque, int version_id)
+{
+    int value = qemu_get_byte(f);
+
+    if (value && !colo_requested) {
+        DPRINTF("COLO requested!\n");
+    }
+    colo_requested = value;
+
+    return 0;
+}
+
+static SaveVMHandlers savevm_colo_info_handlers = {
+    .save_state = colo_info_save,
+    .load_state = colo_info_load,
+};
+
+void colo_info_mig_init(void)
+{
+    register_savevm_live(NULL, "colo", -1, 1,
+                         &savevm_colo_info_handlers, NULL);
+}
