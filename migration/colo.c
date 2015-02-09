550919257ae77c78ab3bc03ad247eef6c53547b8
zhanghailiang <zhang.zhanghailiang@huawei.com>
2015-02-09 17:52:00 +0000

    migration: Integrate COLO checkpoint process into migration
    Add a migrate state: MIGRATION_STATUS_COLO, enter this migration state
    after the first live migration successfully finished.
    
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>
    Signed-off-by: Lai Jiangshan <laijs@cn.fujitsu.com>

--- migration/colo.c
+++ migration/colo.c
@@ -10,9 +10,74 @@
  * later.  See the COPYING file in the top-level directory.
  */
 
+#include "sysemu/sysemu.h"
 #include "migration/migration-colo.h"
+#include "qemu/error-report.h"
+
+#define DEBUG_COLO 0
+
+#define DPRINTF(fmt, ...)                                   \
+    do {                                                    \
+        if (DEBUG_COLO) {                                   \
+            fprintf(stderr, "colo: " fmt , ## __VA_ARGS__); \
+        }                                                   \
+    } while (0)
+
+static QEMUBH *colo_bh;
 
 bool colo_supported(void)
 {
     return true;
+}
+
+bool migrate_in_colo_state(void)
+{
+    MigrationState *s = migrate_get_current();
+    return (s->state == MIGRATION_STATUS_COLO);
+}
+
+static void *colo_thread(void *opaque)
+{
+    MigrationState *s = opaque;
+
+    qemu_mutex_lock_iothread();
+    vm_start();
+    qemu_mutex_unlock_iothread();
+    DPRINTF("vm resume to run\n");
+
+
+    /*TODO: COLO checkpoint savevm loop*/
+
+    migrate_set_state(s, MIGRATION_STATUS_COLO, MIGRATION_STATUS_COMPLETED);
+
+    qemu_mutex_lock_iothread();
+    qemu_bh_schedule(s->cleanup_bh);
+    qemu_mutex_unlock_iothread();
+
+    return NULL;
+}
+
+static void colo_start_checkpointer(void *opaque)
+{
+    MigrationState *s = opaque;
+
+    if (colo_bh) {
+        qemu_bh_delete(colo_bh);
+        colo_bh = NULL;
+    }
+
+    qemu_mutex_unlock_iothread();
+    qemu_thread_join(&s->thread);
+    qemu_mutex_lock_iothread();
+
+    migrate_set_state(s, MIGRATION_STATUS_ACTIVE, MIGRATION_STATUS_COLO);
+
+    qemu_thread_create(&s->thread, "colo", colo_thread, s,
+                       QEMU_THREAD_JOINABLE);
+}
+
+void colo_init_checkpointer(MigrationState *s)
+{
+    colo_bh = qemu_bh_new(colo_start_checkpointer, s);
+    qemu_bh_schedule(colo_bh);
 }
