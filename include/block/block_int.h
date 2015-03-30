a1f3a56c373afaa9798b55044ce54c2e24ffc4dd
Wen Congyang <wency@cn.fujitsu.com>
2015-03-30 14:22:38 +0800

    block: Parse "backing_reference" option to reference existing BDS
    Usage:
    -drive file=xxx,id=Y, \
    -drive file=xxxx,id=X,backing_reference.drive_id=Y,backing_reference.hidden-disk.*
    
    It will create such backing chain:
                   {virtio-blk dev 'Y'}                                      {virtio-blk dev 'X'}
                             |                                                          |
                             |                                                          |
                             v                                                          v
    
        [base] <- [mid] <- ( Y )  <----------------- (hidden target) <--------------- ( X )
    
                             v                              ^
                             v                              ^
                             v                              ^
                             v                              ^
                             >>>> drive-backup sync=none >>>>
    
    X's backing file is hidden-disk, and hidden-disk's backing file is Y.
    Disk Y may be opened or reopened in read-write mode, so A block backup
    job is automatically created: source is Y and target is hidden disk.
    Active disk X, hidden disk, and Y are all on the same AioContext.
    
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Signed-off-by: zhanghailiang <zhang.zhanghailiang@huawei.com>
    Signed-off-by: Gonglei <arei.gonglei@huawei.com>

--- include/block/block_int.h
+++ include/block/block_int.h
@@ -364,6 +364,7 @@ struct BlockDriverState {
     QDict *full_open_options;
     char exact_filename[PATH_MAX];
 
+    bool backing_reference;
     BlockDriverState *backing_hd;
     BlockDriverState *file;
 
