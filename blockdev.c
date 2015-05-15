c8957411852f9920595e9e592e43b36184bfa09b
Wen Congyang <wency@cn.fujitsu.com>
2015-05-15 15:45:29 +0800

    mirror: correct buf_size
    If bus_size is less than 0, the command fails.
    If buf_size is 0, use DEFAULT_MIRROR_BUF_SIZE.
    If buf_size % granularity is not 0, mirror_free_init() will
    do dangerous things.
    
    Signed-off-by: Wen Congyang <wency@cn.fujitsu.com>
    Reviewed-by: Fam Zheng <famz@redhat.com>

--- blockdev.c
+++ blockdev.c
@@ -2619,8 +2619,6 @@ out:
     aio_context_release(aio_context);
 }
 
-#define DEFAULT_MIRROR_BUF_SIZE   (10 << 20)
-
 void qmp_drive_mirror(const char *device, const char *target,
                       bool has_format, const char *format,
                       bool has_node_name, const char *node_name,
@@ -2661,7 +2659,7 @@ void qmp_drive_mirror(const char *device, const char *target,
         granularity = 0;
     }
     if (!has_buf_size) {
-        buf_size = DEFAULT_MIRROR_BUF_SIZE;
+        buf_size = 0;
     }
 
     if (granularity != 0 && (granularity < 512 || granularity > 1048576 * 64)) {
